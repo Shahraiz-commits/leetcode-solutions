635d44496991adb70688f1f40d73c2e2706fd400
Shahraiz-commits <azizshahraiz12@gmail.com>
2025-10-12 11:46:05 -0400

    medium

---
+++ 24-Swap_nodes_in_pairs.py
@@ -0,0 +1,20 @@
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        prev = dummy
+
+        while head and head.next:
+            first = head
+            second = head.next
+
+            # Swapping
+            prev.next = second
+            first.next = second.next
+            second.next = first
+
+            # Move pointers forward
+            prev = first
+            head = first.next
+
+        return dummy.next
+
